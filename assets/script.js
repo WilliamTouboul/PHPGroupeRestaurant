00f612e980516455f4ceab63eaba522683da0cfa
Douyere Nicolas <nicolasdouyere.pro@gmail.com>
2021-01-11 15:54:41 +0100

    modification pour menu du JS

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+$(document).on('mouseover', '.ndContentImage', function (e) {
+    $('img', this).toggleClass('ndContentImageHover');
+});
+
+$(document).on('mouseout', '.ndContentImage', function (e) {
+    $('img', this).toggleClass('ndContentImageHover');
+});
+
+
+
+
+
+
+
